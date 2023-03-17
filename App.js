7807804b79e072a70bb0d4cc977e394a5068b064
SHRINES K <128037088+shrikann@users.noreply.github.com>
2023-03-17 16:08:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import World from './component/World';
+  function App() {
+  return (
+    <div className="App">
+          <World/>
+    </div>
+  );
+}
+
+export default App;
---
+++ World.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function World() {
+  return (
+    <div><p>This is world Functional component</p></div>
+  )
+}
+
+export default World
